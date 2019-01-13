dd3aea66edac64fc209800b1703579b77e6a0193
Zachary Mixon <admin@stupidrobot.club>
2019-01-13 01:44:59 -0700

    Update SQL to more efficiently deal with timestamps

--- sql/create_procedures.sql
+++ sql/create_procedures.sql
@@ -1,13 +1,24 @@
 DELIMITER //
+CREATE PROCEDURE `endSession`()
+BEGIN
+
+  UPDATE sessions
+  SET end_timestamp = CURRENT_TIMESTAMP
+  WHERE ISNULL(end_timestamp);
+
+END//
+
+DELIMITER //
 CREATE PROCEDURE `newSession`(
   IN streamer_in VARCHAR(256),
   IN activity_in VARCHAR(256),
-  IN start_timestamp_in INT,
   IN raw_topic_string_in VARCHAR(512),
   IN activity_raw_in VARCHAR(16))
 BEGIN
 
   DECLARE activity_id_returned INT;
+
+  CALL endSession();
 
   SELECT activities_mapping_id
   INTO activity_id_returned
@@ -18,12 +29,13 @@ BEGIN
   THEN -- INSERT
     INSERT INTO activities_mapping(activity_raw)
     VALUES (activity_raw_in);
-    INSERT INTO sessions(streamer, activity, start_timestamp, raw_topic_string, fk_activities_mapping)
-    VALUES (streamer_in, activity_in, start_timestamp_in, raw_topic_string_in, LAST_INSERT_ID());
+    INSERT INTO sessions(streamer, activity, raw_topic_string, fk_activities_mapping)
+    VALUES (streamer_in, activity_in, raw_topic_string_in, LAST_INSERT_ID());
   ELSE -- USE RETURNED VALUE
-    INSERT INTO sessions(streamer, activity, start_timestamp, raw_topic_string, fk_activities_mapping)
-    VALUES (streamer_in, activity_in, start_timestamp_in, raw_topic_string_in, activity_id_returned);
+    INSERT INTO sessions(streamer, activity, raw_topic_string, fk_activities_mapping)
+    VALUES (streamer_in, activity_in, raw_topic_string_in, activity_id_returned);
   END IF;
 
+  SELECT LAST_INSERT_ID() AS `session_id`;
+
 END //
-DELIMITER;
