1e486c8823f9cdcbeaf29ede08a73afd18c541bb
Zachary Mixon <admin@stupidrobot.club>
2019-01-20 00:01:28 -0700

    Remove SQL Comments

--- index.js
+++ index.js
@@ -34,23 +34,6 @@ let forceKill = false
 **  Functions
 */
 
-/*
-** SQL COMMANDS
-** LastPlayed: select * from sessions_view limit 1
-** FirstPlayed: select * from sessions_view order by session_id asc limit 1
-** TotalPlayed: call totalSession('%activity%', '%streamer%')
-** totalSession requires you to pass '%%' as defaults, mysql doesn't
-** allow for default parameters apparently.
-**
-** Unmapped Activities Count: select COUNT(activity_raw) as `Unmapped Activities Count` from activities_mapping where ISNULL(fk_activity_types)
-** This is used to alert the operator if there's any wacky new activity types that aren't
-** yet mapped to one of the few "normal" activities
-**
-** Start session: call newSession(streamer, activity, raw_topic, activity_type)
-** Start session returns session_id and will call endSession() before starting a new one
-** End session: call endSession()
-*/
-
 const leftPad = (str, amount = 2) => {
   str = str + ''
   const out = [...str]
