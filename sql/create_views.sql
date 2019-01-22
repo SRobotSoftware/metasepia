29f436e8e8a148c114fdd942682f5f53d0be5261
Zachary Mixon <admin@stupidrobot.club>
2019-01-22 01:16:08 -0700

    Update session_view for more accuracy

--- sql/create_views.sql
+++ sql/create_views.sql
@@ -1,14 +1,15 @@
-CREATE VIEW sessions_view
-AS select
-sessions.session_id,
-sessions.streamer,
-sessions.activity,
-sessions.start_timestamp,
-sessions.end_timestamp,
-(UNIX_TIMESTAMP(sessions.end_timestamp) - UNIX_TIMESTAMP(sessions.start_timestamp)) AS `duration_in_seconds`,
-activity_types.activity_type
-from
-sessions
-left join activities_mapping on sessions.fk_activities_mapping = activities_mapping.activities_mapping_id
-left join activity_types on activities_mapping.fk_activity_types = activity_types.activity_types_id
-ORDER BY sessions.session_id DESC;
+ALTER VIEW sessions_view AS SELECT
+MAX(s.session_id) AS session_id,
+s.streamer,
+s.activity,
+MIN(s.start_timestamp) AS start_timestamp,
+MAX(s.end_timestamp) AS end_timestamp,
+(UNIX_TIMESTAMP(MAX(s.end_timestamp)) - UNIX_TIMESTAMP(MIN(s.start_timestamp))) AS `duration_in_seconds`,
+act.activity_type,
+FLOOR(UNIX_TIMESTAMP(s.start_timestamp) / 86400) AS `day`
+FROM
+sessions AS s
+LEFT JOIN activities_mapping ON s.fk_activities_mapping = activities_mapping.activities_mapping_id
+LEFT JOIN activity_types AS act ON activities_mapping.fk_activity_types = act.activity_types_id
+GROUP BY streamer, activity, activity_type, `day`
+ORDER BY session_id DESC;
