38fe0231b15fd67495003bfbbbf86259973d8ae8
Zachary Mixon <admin@stupidrobot.club>
2019-01-22 23:12:16 -0700

    Subtract the smaller from the larger, fixing the currentlyplaying diff

--- index.js
+++ index.js
@@ -272,7 +272,7 @@ const currentlyPlaying = (from, to, message, opts) => {
 
       res = res[0]
 
-      const duration = moment.duration(moment(res.start_timestamp).diff(moment(), 'milliseconds'), 'milliseconds')
+      const duration = moment.duration(moment().diff(moment(res.start_timestamp), 'milliseconds'), 'milliseconds')
       const response = (res.end_timestamp) ? 'Nobody is currently streaming.' : `${res.streamer} has been streaming the ${res.activity_type} ${res.activity} for ${leftPad(duration.get('hours'))}:${leftPad(duration.get('minutes'))}:${leftPad(duration.get('seconds'))}`
       const output = `${from}: ${response}`
       send(to, output, opts)
